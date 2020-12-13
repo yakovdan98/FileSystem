22d1ba1dcd41032e83215f37a64e035d19e0cb23
a-montgomery <72419621+a-montgomery@users.noreply.github.com>
2020-12-13 15:29:58 -0800

    Update FileTableEntry.java

--- FileTableEntry.java
+++ FileTableEntry.java
@@ -1,45 +1,18 @@
-import java.util.Vector;
+public class FileTableEntry {           // Each table entry should have
+    public int seekPtr;                 // a file seek pointer
+    public final Inode inode;           // a reference to its inode
+    public final short iNumber;         // this inode number
+    public int count;                   // # threads sharing this entry
+    public final String mode;           // "r", "w", "w+", or "a"
 
-public class FileTable {
-
-    private Vector table;
-    private Directory dir;
-
-    public FileTable(Directory directory)
+    public FileTableEntry(Inode i, short inumber, String m)
     {
-        table = new Vector();       // instantiate a file (structure) table
-        dir = directory;            // receive a reference to the Directory
-    }                               // from the file system
-
-    //major public methods
-    public synchronized FileTableEntry falloc(String filename, String mode)
-    {
-        short iNumber;
-        Inode iNode;
-
-        while(true)
-        {
-            //  iNumber = (short) filename.equals("/");
-
-        }
-        // allocate a new file (structure) table entry for this file name
-        // allocate/retrieve and register the corresponding inode using dir
-        // increment this inode's count
-        // immediately write back this inode to the disk
-        // return a reference to this file (structure) table entry
+        seekPtr = 0;                    // the seek pointer is set to the file top
+        inode = i;
+        iNumber = inumber;
+        count = 1;                      // at least one thread is using this entry
+        mode = m;                       // once access mode is set, it never changes
+        if(mode.compareTo("a") == 0)    // if mode is append,
+            seekPtr = inode.length;     // seekPtr points to the end of the file
     }
-
-    public synchronized boolean ffree(FileTableEntry e)
-    {
-        // receive a file table entry reference
-        // save the correspond inode to the disk
-        //free this file table entry
-        // return true if this file table entry found in my table
-    }
-
-    public synchronized boolean fempty()
-    {
-        return table.isEmpty();     // return if table is empty
-    }                               // should be called before starting a format
-
 }
