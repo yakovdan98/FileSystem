bebc3a987c16c993b9577c2ac4fba9dc1c04c049
yakovdan98 <59814766+yakovdan98@users.noreply.github.com>
2020-12-16 20:51:36 -0800

    comments

--- Directory.java
+++ Directory.java
@@ -1,11 +1,12 @@
-/*
- * This class represents the Directory of the file system. This implementation is a single-level design and consists
- * of the "/" root directory.  This is the only directory for the file system.
+import java.util.Arrays;
+
+/**
+ *  Directory class that holds the different file names that are stored on the disk and the inode number for them
  */
 
 public class Directory {
     private static int maxChars = 30; // max characters of each file name
-    private static final int FILE_ENTRY = 64;
+    private static final int FILE_ENTRY = 64; //max size of each file
 
     // Directory entries
     private int fsize[];        // each element stores a different file size.
@@ -21,18 +22,23 @@ public class Directory {
         root.getChars(0, fsize[0], fnames[0], 0); // fnames[0] includes "/"
     }
 
+    /**
+     * takes an array of bytes of data and initializes it in the directory
+     * @param data data to be turned into directory
+     * @return 1 if succesfully completed
+     */
     public int bytes2directory(byte data[]) {
         // assumes data[] received directory information from disk
         // initializes the Directory instance with this data[]
         int index = 0;
 
         for(int i = 0; i < fsize.length; i++){
-            fsize[i] = SysLib.bytes2int(data, i);
+            fsize[i] = SysLib.bytes2int(data, i); //set the size of file
             index += 4;
 
-            String name = new String(data, index, maxChars * 2);
-            name.getChars(0, fsize[i], fnames[i], 0);
-            index += maxChars * 2;
+            String name = new String(data, index, maxChars * 2); //turn byte data into string
+            name.getChars(0, fsize[i], fnames[i], 0);//turn copy string chars into fname array
+            index += maxChars * 2;//increases index by 60
 
 
 
@@ -40,6 +46,10 @@ public class Directory {
         return 1;
     }
 
+    /**
+     * takes the directory and turns it into data, an array of bytes
+     * @return byte array of data
+     */
     public byte[] directory2bytes() {
         // converts and return Directory information into a plain byte array
         // this byte array will be written back to disk
@@ -49,40 +59,45 @@ public class Directory {
         byte[] out = new byte[FILE_ENTRY * fsize.length];
 
         for(int i = 0; i < fsize.length; i++){
-            SysLib.int2bytes(fsize[i], out, index);
+            SysLib.int2bytes(fsize[i], out, index);//turn size into byte data
             index += 4;
 
-            String name = new String(fnames[i], 0, maxChars);
-            byte[] tmp = name.getBytes();
-            System.arraycopy(tmp, 0, out, index, tmp.length);
+            String name = new String(fnames[i], 0, maxChars);//create string from char array
+            byte[] tmp = name.getBytes();//make byte array out of string
+            System.arraycopy(tmp, 0, out, index, tmp.length);//copy byte array into the data array
             index += 60;
         }
         return out;
     }
 
+    /**
+     * alocates a new file into the directory
+     * @param filename name of new file to be created
+     * @return inode number of new file
+     */
     public short ialloc(String filename) {
         // filename is the one of a file to be created.
         // allocates a new inode number for this filename
         int inode = 0;
         boolean found = false;
 
-        for (int i = 1; i < fsize.length; i++) {
+        for (int i = 1; i < fsize.length; i++) {//search for empty spot
 
-            if (filename.length() == fsize[i]) {
+            if (filename.length() == fsize[i]) {//see if index filename matches name of file that is being created
                 char buff[] = new char[fsize[i]];
                 for (int j = 0; j < fsize[i]; j++) {
                     buff[j] = fnames[i][j];
                 }
                 if(filename.equals(new String(buff))){
-                    found = true;
+                    found = true;//filename already exists
                 }
             }
 
-            if (fsize[i] == 0 && inode == 0) {
+            if (fsize[i] == 0 && inode == 0) {//find fist open slot
                 inode = i;
             }
 
-            if(!found && inode != 0){
+            if(!found && inode != 0){//only create new file if no file with same name was found
                 fsize[inode] = filename.length();
                 filename.getChars(0, fsize[inode], fnames[inode], 0);
                 return (short)inode;
@@ -91,36 +106,41 @@ public class Directory {
         return -1;
     }
 
+    /**
+     * free's up a directory slot
+     * @param iNumber inode number to free up
+     * @return true if slot was freed
+     */
     public boolean ifree(short iNumber) {
         // deallocates this inumber (inode number)
         // the corresponding file will be deleted.
         fsize[iNumber] = 0;
-        for(int i = 0; i < maxChars; i++)
-        {
-            fnames[iNumber][i] = '0';
-        }
-
         return true;
     }
 
+    /**
+     * finds the inode number for a file in directory
+     * @param filename name to search up
+     * @return inode number, -1 if file not found
+     */
     public short namei(String filename) {
         // returns the inumber corresponding to this filename
 
-        for (int i = 1; i < fsize.length; i++) {
+        for (int i = 1; i < fsize.length; i++) {//go through directory
 
-            if (filename.length() == fsize[i]) {
+            if (filename.length() == fsize[i]) {//if lengths match
                 char buff[] = new char[fsize[i]];
 
-                for (int j = 0; j < fsize[i]; j++) {
+                for (int j = 0; j < fsize[i]; j++) {//create buffer char array
                     buff[j] = fnames[i][j];
                 }
 
-                if(filename.equals(new String(buff))){
-                    return (short)i;
+                if(filename.equals(new String(buff))){//turn buffer array into string and compare
+                    return (short)i;//return inode num
                 }
             }
 
         }
-        return -1;
+        return -1;//if file not found
     }
 }
--- FileTable.java
+++ FileTable.java
@@ -1,4 +1,11 @@
+import java.util.IdentityHashMap;
 import java.util.Vector;
+
+/**
+ * File Table class is a container for a list of currently open files. This object will store opened files
+ * on a vector and remove them once they are closed.
+ */
+
 
 public class FileTable {
 
@@ -11,13 +18,20 @@ public class FileTable {
     }                             // from the file system
 
     // major public methods
+
+    /**
+     * allocates a a table entry for a file
+     * @param filename file to be allocated
+     * @param mode mode that the file is opened as
+     * @return file table entry that has been created
+     */
     public synchronized FileTableEntry falloc( String filename, String mode ) {
         // allocate a new file (structure) table entry for this file name
         // allocate/retrieve and register the corresponding inode using dir
 
 
 
-        short iNumber = filename.equals("/") ? 0 : dir.namei(filename);
+        short iNumber = filename.equals("/") ? 0 : dir.namei(filename); //search for file
         Inode inode;
 
         while(true){
@@ -25,31 +39,33 @@ public class FileTable {
             if(iNumber > 0){
                 inode = new Inode(iNumber);
 
-                if(mode.equals("r")){
+                if(mode.equals("r")){//read flag
                     inode.flag = 3; //read
                     break;
                 }
-                else{
+                else{//write flag
                     inode.flag = 4; //write
                     break;
                 }
 
             }
 
-            if(!mode.equals("r")){
+            if(!mode.equals("r")){//read and write or append
                 iNumber = dir.ialloc(filename);
                 inode = new Inode(iNumber);
                 inode.flag = 3; //read
                 break;
 
             }
-            return null;
+            return null;//unrecognizable command
 
         }
         // increment this inode's count
         inode.count++;
+
         // immediately write back this inode to the disk
         inode.toDisk(iNumber);
+
         // return a reference to this file (structure) table entry
         FileTableEntry entry = new FileTableEntry(inode, iNumber, mode);
         table.addElement(entry);
@@ -57,20 +73,47 @@ public class FileTable {
 
     }
 
+    /**
+     * free up table of entry
+     * @param e entry to be freed
+     * @return if entry was succesfully freed
+     */
     public synchronized boolean ffree( FileTableEntry e ) {
         // receive a file table entry reference
-        // save the corresponding inode to the disk
-        e.inode.toDisk(e.iNumber);
+
+        Inode inode = new Inode(e.iNumber);
         // free this file table entry.
         // return true if this file table entry found in my table
 
         if(table.remove(e)) {
-            e.inode.count--;
+            switch (inode.flag){
+                case 3:
+                    if(inode.count == 1){ inode.flag = 1; } //used
+                    break;
+
+                case 4:
+                    inode.flag = 1;//used
+                    break;
+
+                default:
+                    throw new IllegalStateException("Unexpected value: " + inode.flag);
+            }
+
+            //decrease count
+            inode.count--;
+
+            // save the corresponding inode to the disk
+            inode.toDisk(e.iNumber);
             return true;
+
         }
         return false;
     }
 
+    /**
+     * check if the table is empty
+     * @return true table empty, false table has entries
+     */
     public synchronized boolean fempty( ) {
         return table.isEmpty( );  // return if table is empty
     }                            // should be called before starting a format
